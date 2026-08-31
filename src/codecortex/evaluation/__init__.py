cc246d94c75d8fea8bfb065e6e2c92fbea01e1d2
Behnam Jalali <behnamjalalico@gmail.com>
2026-08-31 04:34:25 +0330

    Add benchmark history and regression gates

---
+++ src/codecortex/evaluation/__init__.py
@@ -0,0 +1,17 @@
+"""Benchmark history and regression policy."""
+
+from codecortex.evaluation.regression import (
+    BenchmarkHistory,
+    BenchmarkSnapshot,
+    GateReport,
+    MetricPolicy,
+    RegressionGate,
+)
+
+__all__ = [
+    "BenchmarkHistory",
+    "BenchmarkSnapshot",
+    "GateReport",
+    "MetricPolicy",
+    "RegressionGate",
+]
