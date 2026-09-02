fc9185555bbcfd5ec78f96197947eb99d3252772
Behnam Jalali <behnamjalalico@gmail.com>
2026-09-02 16:43:16 +0330

    platform 02: enforce transport-neutral platform contracts

---
+++ src/codecortex/application/__init__.py
@@ -0,0 +1,5 @@
+"""Transport-neutral application services for CodeCortex."""
+
+from codecortex.application.contracts import MeasurementKind, MeasurementValue, Page
+
+__all__ = ["MeasurementKind", "MeasurementValue", "Page"]
